9b404c54f268e003186ccf58e5dfc1369d4d23e7
Kaiyu Yan <kyan9337@bu.edu>
2018-10-08 18:48:07 -0400

    ky

--- Midterm Project.R
+++ Midterm Project.R
@@ -14,7 +14,7 @@ Read_All_sheet <- function(filename, tibble = FALSE) {
 
 
 # Read all worksheet from excel
-Attendence <- Read_All_sheet("RedSox Attendance 2012-2018.xlsx")
+Attendence <- Read_All_sheet("RedSoxAttendance 2012-2018.xlsx")
 
 # Read and select home game from 2018 Dataset
 Game_2018 <- select(Attendence[["2018"]], Tm:Opp, contains("D/N"), Attendance)
@@ -22,12 +22,13 @@ Clean_2018 <- filter(Game_2018, is.na(X__2))
 
 # Read and select home game from 2017 Dataset
 Game_2017 <- select(Attendence[["2017"]], Tm:Opp, contains("D/N"), Attendance)
-Clean_2017 <- filter(Game_2017, is.na(X__2))
+Clean_2017 <- filter(Game_2017, is.na(X__2) )
 
 # Read and select home game from 2016 Dataset
 Game_2016 <- select(Attendence[["2016"]], Tm:Opp, contains("D/N"), Attendance)
-Clean_2016 <- filter(Game_2016, is.na(X__2)) %>%
-  na.omit(Clean_2016)
+Clean_2016 <- filter(Game_2016, is.na(X__2) ) 
+Clean_2016 <-  Clean_2016[1:81,]
+
 # Read and select home game from 2015 Dataset
 Game_2015 <- select(Attendence[["2015"]], Tm:Opp, contains("D/N"), Attendance)
 Clean_2015 <- filter(Game_2015, is.na(X__2))
