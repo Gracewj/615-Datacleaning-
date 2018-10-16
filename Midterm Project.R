9d2e672c8b377009aeeaaa3bd323e7a7c7e40852
Kaiyu Yan <kyan9337@bu.edu>
2018-10-16 18:56:06 -0400

    ky

--- Midterm Project.R
+++ Midterm Project.R
@@ -1,30 +1,31 @@
-library(readxl)
+library(xml2)
+library(rvest)
 library(tidyverse)
 
-# Create function that could reald all tabs from excel
-Read_All_sheet <- function(filename, tibble = FALSE) {
-  sheets <- readxl::excel_sheets(filename)
-  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
-  if (!tibble) {
-    x <- lapply(x, as.data.frame)
-  }
-  names(x) <- sheets
-  x
-}
 
+site1 <- "https://www.baseball-reference.com/teams/BOS/"
+site2 <- "-schedule-scores.shtml"
+year <- c(2012:2017)
+site <- paste0(site1,year,site2)
 
-# Read all worksheet from excel
-Attendence <- Read_All_sheet("RedSoxAttendance 2012-2018.xlsx")
+Raw_2012<- as.data.frame(read_html(site[1]) %>% html_nodes("table") %>% html_table())
+Raw_2013<- as.data.frame(read_html(site[2]) %>% html_nodes("table") %>% html_table())
+Raw_2014<- as.data.frame(read_html(site[3]) %>% html_nodes("table") %>% html_table())
+Raw_2015<- as.data.frame(read_html(site[4]) %>% html_nodes("table") %>% html_table())
+Raw_2016<- as.data.frame(read_html(site[5]) %>% html_nodes("table") %>% html_table())
+Raw_2017<- as.data.frame(read_html(site[6]) %>% html_nodes("table") %>% html_table())
+
 weather_data <- read.csv("weather data.csv")
 
 
 # Read and select home game from 2017 Dataset
-Game_2017 <- select(Attendence[["2017"]], Date, Tm:Opp, contains("D/N"), Attendance)
+Game_2017 <- select(Raw_2017, Date, Tm:Opp, contains("D/N"), Attendance)
 Data_2017<- data.frame(do.call('rbind', strsplit(as.character(Game_2017$Date),',',fixed=TRUE)))
 Data_2017<- data.frame(do.call('rbind', strsplit(as.character(Data_2017$X2),' ',fixed=TRUE)))
 Game_2017 <- merge(Game_2017,Data_2017,by = 0)
-Clean_2017 <- filter(Game_2017, is.na(X__2))
-C_2017 <- select(Clean_2017, 7,9,10)
+Clean_2017 <- filter(Game_2017, !str_detect(Var.5,"@"))
+Clean_2017 <- filter(Clean_2017,str_detect(Tm,"BOS"))
+C_2017 <- select(Clean_2017, 6,8,9)
 C_2017$X2 <- match(C_2017$X2,month.abb)
 C_2017$year <- rep(2017,nrow(C_2017)) # make new column 
 A <- C_2017[,c(1,4,2,3)]
@@ -33,12 +34,13 @@ C_2017$Date <- as.Date(C_2017$Date,"%Y-%m-%d")
 
 
 # Read and select home game from 2016 Dataset
-Game_2016 <- select(Attendence[["2016"]], Date, Tm:Opp, contains("D/N"), Attendance)
+Game_2016 <- select(Raw_2016, Date, Tm:Opp, contains("D/N"), Attendance)
 Data_2016<- data.frame(do.call('rbind', strsplit(as.character(Game_2016$Date),',',fixed=TRUE)))
 Data_2016<- data.frame(do.call('rbind', strsplit(as.character(Data_2016$X2),' ',fixed=TRUE)))
 Game_2016 <- merge(Game_2016,Data_2016,by = 0)
-Clean_2016 <- filter(Game_2016, is.na(X__2))
-C_2016 <- select(Clean_2016, 7,9,10)
+Clean_2016 <- filter(Game_2016, !str_detect(Var.5,"@"))
+Clean_2016 <- filter(Clean_2016,str_detect(Tm,"BOS"))
+C_2016 <- select(Clean_2016, 6,8,9)
 C_2016$X2 <- match(C_2016$X2,month.abb)
 C_2016$year <- rep(2016,nrow(C_2016)) # make new column 
 A <- C_2016[,c(1,4,2,3)]
@@ -47,12 +49,13 @@ C_2016$Date <- as.Date(C_2016$Date,"%Y-%m-%d")
 C_2016 <- na.omit(C_2016)
 
 # Read and select home game from 2015 Dataset
-Game_2015 <- select(Attendence[["2015"]], Date, Tm:Opp, contains("D/N"), Attendance)
+Game_2015 <- select(Raw_2015, Date, Tm:Opp, contains("D/N"), Attendance)
 Data_2015<- data.frame(do.call('rbind', strsplit(as.character(Game_2015$Date),',',fixed=TRUE)))
 Data_2015<- data.frame(do.call('rbind', strsplit(as.character(Data_2015$X2),' ',fixed=TRUE)))
 Game_2015 <- merge(Game_2015,Data_2015,by = 0)
-Clean_2015 <- filter(Game_2015, is.na(X__2))
-C_2015 <- select(Clean_2015, 7,9,10)
+Clean_2015 <- filter(Game_2015, !str_detect(Var.5,"@"))
+Clean_2015 <- filter(Clean_2015,str_detect(Tm,"BOS"))
+C_2015 <- select(Clean_2015, 6,8,9)
 C_2015$X2 <- match(C_2015$X2,month.abb)
 C_2015$year <- rep(2015,nrow(C_2015)) # make new column 
 A <- C_2015[,c(1,4,2,3)]
@@ -61,12 +64,13 @@ C_2015$Date <- as.Date(C_2015$Date,"%Y-%m-%d")
 
 
 # Read and select home game from 2014 Dataset
-Game_2014 <- select(Attendence[["2014"]], Date, Tm:Opp, contains("D/N"), Attendance)
+Game_2014 <- select(Raw_2014, Date, Tm:Opp, contains("D/N"), Attendance)
 Data_2014<- data.frame(do.call('rbind', strsplit(as.character(Game_2014$Date),',',fixed=TRUE)))
 Data_2014<- data.frame(do.call('rbind', strsplit(as.character(Data_2014$X2),' ',fixed=TRUE)))
 Game_2014 <- merge(Game_2014,Data_2014,by = 0)
-Clean_2014 <- filter(Game_2014, is.na(X__2))
-C_2014 <- select(Clean_2014, 7,9,10)
+Clean_2014 <- filter(Game_2014, !str_detect(Var.5,"@"))
+Clean_2014 <- filter(Clean_2014,str_detect(Tm,"BOS"))
+C_2014 <- select(Clean_2014, 6,8,9)
 C_2014$X2 <- match(C_2014$X2,month.abb)
 C_2014$year <- rep(2014,nrow(C_2014)) # make new column 
 A <- C_2014[,c(1,4,2,3)]
@@ -75,12 +79,13 @@ C_2014$Date <- as.Date(C_2014$Date,"%Y-%m-%d")
 
 
 # Read and select home game from 2013 Dataset
-Game_2013 <- select(Attendence[["2013"]], Date, Tm:Opp, contains("D/N"), Attendance)
+Game_2013 <- select(Raw_2013, Date, Tm:Opp, contains("D/N"), Attendance)
 Data_2013<- data.frame(do.call('rbind', strsplit(as.character(Game_2013$Date),',',fixed=TRUE)))
 Data_2013<- data.frame(do.call('rbind', strsplit(as.character(Data_2013$X2),' ',fixed=TRUE)))
 Game_2013 <- merge(Game_2013,Data_2013,by = 0)
-Clean_2013 <- filter(Game_2013, is.na(X__2))
-C_2013 <- select(Clean_2013, 7,9,10)
+Clean_2013 <- filter(Game_2013, !str_detect(Var.5,"@"))
+Clean_2013 <- filter(Clean_2013,str_detect(Tm,"BOS"))
+C_2013 <- select(Clean_2013, 6,8,9)
 C_2013$X2 <- match(C_2013$X2,month.abb)
 C_2013$year <- rep(2013,nrow(C_2013)) # make new column 
 A <- C_2013[,c(1,4,2,3)]
@@ -88,12 +93,13 @@ C_2013 <- unite(A, Date,2:4, sep = "-", remove = TRUE)
 C_2013$Date <- as.Date(C_2013$Date,"%Y-%m-%d")
 
 # Read and select home game from 2012 Dataset
-Game_2012 <- select(Attendence[["2012"]], Date, Tm:Opp, contains("D/N"), Attendance)
+Game_2012 <- select(Raw_2012, Date, Tm:Opp, contains("D/N"), Attendance)
 Data_2012<- data.frame(do.call('rbind', strsplit(as.character(Game_2012$Date),',',fixed=TRUE)))
 Data_2012<- data.frame(do.call('rbind', strsplit(as.character(Data_2012$X2),' ',fixed=TRUE)))
 Game_2012 <- merge(Game_2012,Data_2012,by = 0)
-Clean_2012 <- filter(Game_2012, is.na(X__2))
-C_2012 <- select(Clean_2012, 7,9,10)
+Clean_2012 <- filter(Game_2012, !str_detect(Var.5,"@"))
+Clean_2012 <- filter(Clean_2012,str_detect(Tm,"BOS"))
+C_2012 <- select(Clean_2012, 6,8,9)
 C_2012$X2 <- match(C_2012$X2,month.abb)
 C_2012$year <- rep(2012,nrow(C_2012)) # make new column 
 A <- C_2012[,c(1,4,2,3)]
@@ -199,3 +205,5 @@ ggplot(Table_2017,aes( x = Date, y = Attendance, size = TAVG)) +
   ggtitle("Weather vs attendance 2017") +
   xlab("Date") +
   ylab("attendance")
+
+#########################
